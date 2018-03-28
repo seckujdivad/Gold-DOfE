c32e6d346231d4f0e28c37e0d1755a9e946e3b29
David Juckes <david.a.juckes@tesco.net>
2018-03-28 21:27:00 +0100

    Server connects to client

--- modules/engine.py
+++ modules/engine.py
@@ -10,10 +10,11 @@ class Game:
         class server:
             mode = 'internal' #either 'internal' or 'external'
             allow_external = True
+            name = 'localhost'
         self.server = server
         
         self.running = True        
-        threading.Thread(target = self.main).start()
+        threading.Thread(target = self.main, daemon = True).start()
     
     def main(self):
         while self.running:
@@ -23,8 +24,13 @@ class Game:
         'Connect to a server. If the hostname is None, a server will be created'
         if hostname == None:
             self.server.mode = 'internal'
+            self.server.name = 'localhost'
+            self.server.object = Server(4321)
         else:
             self.server.mode = 'external'
+            self.server.name = hostname
+        self.client = Client(self.server.name, 4321)
+        self.client.send_raw('hello world!')
     
     def close(self):
         self.running = False
@@ -37,25 +43,43 @@ class Water:
 
 class Player:
     pass
-
     
 class Server:
-    def __init__(self, port):
+    def __init__(self, port_):
         class serverdata:
-            host = socket.gethostname()
-            port = port
+            host = ''
+            port = port_
         self.serverdata = serverdata
         
-        self.connection = socket.socket(socket.AF_INET, socket.STREAM)
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connection.bind((self.serverdata.host, self.serverdata.port))
         self.connection.listen(5)
+        threading.Thread(target = self.acceptance_thread, name = 'Acceptance thread', daemon = True).start()
+        
+    def acceptance_thread(self):
+        while True:
+            print('ready')
+            conn, addr = self.connection.accept()
+            print(addr)
+            threading.Thread(target = self.connection_handler, args = [addr, conn], daemon = True).start()
+    
+    def connection_handler(self, address, connection):
+        print(address)
+        while True:
+            data = connection.recv(2048)
+            print(data.decode('UTF-8'))
 
 class Client:
-    def __init__(self, host, port):
+    def __init__(self, host_, port_):
         class serverdata:
-            host = host
-            port = port
+            host = host_
+            port = port_
         self.serverdata = serverdata
         
-        self.connection = socket.socket(socket.AF_INET, socket.STREAM)
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print('xd')
         self.connection.connect((self.serverdata.host, self.serverdata.port))
+        print('2')
+    
+    def send_raw(self, text):
+        self.connection.send(text.encode())
