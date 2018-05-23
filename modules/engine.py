d0af445d232d78790c74f098c5f4cc1b6315eaa9
David Juckes <david.a.juckes@tesco.net>
2018-05-23 09:58:36 +0100

    Changed scatters to use new model based display method

--- modules/engine.py
+++ modules/engine.py
@@ -1,4 +1,3 @@
-from PIL import ImageTk
 import tkinter as tk
 import sqlite3 as sql
 import time
@@ -59,10 +58,19 @@ class Engine:
                 obj_overlay = None
             name = None
             cfg = None
+            rendermethod = None
         self.map = map
     
     def load_map(self, name):
         if os.path.isdir(os.path.join(sys.path[0], 'server', 'maps', name)):
+            with open(os.path.join(sys.path[0], 'user', 'config.json'), 'r') as file:
+                self.map.settingscfg = json.load(file)
+                
+            if self.map.settingscfg['graphics']['PILrender']:
+                self.map.rendermethod = __import__('PIL').ImageTk.PhotoImage
+            else:
+                self.map.rendermethod = tk.PhotoImage
+        
             if not self.map.textures.obj_scatter == []:
                 for scatter in self.map.textures.obj_scatter:
                     self.game.canvas.delete(scatter)
@@ -75,16 +83,16 @@ class Engine:
             with open(os.path.join(sys.path[0], 'server', 'maps', name, 'list.json'), 'r') as file:
                 self.map.cfg = json.load(file)
             
-            self.map.textures.base = ImageTk.PhotoImage(file = os.path.join(sys.path[0], 'server', 'maps', name, self.map.cfg['background']['base']))
-            self.map.textures.overlay = ImageTk.PhotoImage(file = os.path.join(sys.path[0], 'server', 'maps', name, self.map.cfg['background']['overlay']))
-            for scatter in self.map.cfg['background']['scatters']:
-                self.map.textures.scatters.append(ImageTk.PhotoImage(file = os.path.join(sys.path[0], 'server', 'maps', name, scatter)))
+            self.map.textures.base = self.map.rendermethod(file = os.path.join(sys.path[0], 'server', 'maps', name, self.map.cfg['background']['base']))
+            self.map.textures.overlay = self.map.rendermethod(file = os.path.join(sys.path[0], 'server', 'maps', name, self.map.cfg['background']['overlay']))
             
             self.map.textures.obj_base = self.game.canvas.create_image(400, 300, image = self.map.textures.base)
             
-            for i in range(int(self.map.cfg['background']['scatternum'] / len(self.map.textures.scatters))):
-                for scatter in self.map.textures.scatters:
-                    self.map.textures.obj_scatter.append(self.game.canvas.create_image(random.randint(0, 800), random.randint(0, 600), image = scatter))
+            for i in range(int(self.map.cfg['background']['scatternum'] / len(self.map.cfg['background']['scatters']))):
+                for scatter in self.map.cfg['background']['scatters']:
+                    scattermdl = Model(os.path.join(sys.path[0], 'server', 'maps', name, scatter), self.map.rendermethod, self.game.canvas)
+                    scattermdl.setpos(random.randint(0, 800), random.randint(0, 600))
+                    self.map.textures.obj_scatter.append(scattermdl)
             
             self.map.textures.obj_overlay = self.game.canvas.create_image(400, 300, image = self.map.textures.overlay)
 
@@ -93,6 +101,49 @@ class Water:
 
 class Player:
     pass
+
+class Model:
+    def __init__(self, path, imageloader, canvas):
+        self.path = path
+        self.imageloader = imageloader
+        self.canvas = canvas
+        
+        class graphics:
+            x = 0
+            y = 0
+            displaytype = None
+            class flat:
+                texture = None
+                imgobj = None
+            class stack:
+                pass
+        self.graphics = graphics
+        
+        with open(os.path.join(self.path, 'list.json'), 'r') as file:
+            self.config = json.load(file)
+        
+        self.graphics.displaytype = self.config['type']
+        if self.graphics.displaytype == 'flat':
+            self.graphics.flat.texture = self.imageloader(file = os.path.join(self.path, self.config['texture']))
+        else:
+            raise ValueError('Display type \'{}\' doesn\'t exist'.format(self.config['type']))
+        self._render()
+    
+    def _render(self, x = None, y = None):
+        if not x == None:
+            self.graphics.x = x
+        if not y == None:
+            self.graphics.y = y
+        if self.graphics.displaytype == 'flat':
+            self.graphics.flat.imgobj = self.canvas.create_image(self.graphics.x, self.graphics.y, image = self.graphics.flat.texture)
+    
+    def setpos(self, x = None, y = None):
+        if not x == None:
+            self.graphics.x = x
+        if not y == None:
+            self.graphics.y = y
+        if self.graphics.displaytype == 'flat':
+            self.canvas.coords(self.graphics.flat.imgobj, self.graphics.x, self.graphics.y)
 
 class DBAccess:
     def __init__(self, address):
