aba7fe6de0c3527631990278881a3f3b7d204cc0
seckujdivad <David.a.juckes@tesco.net>
2018-05-27 23:09:26 +0100

    Rewrote a lot of the engine code after npp deleted it

--- modules/engine.py
+++ modules/engine.py
@@ -49,28 +49,31 @@ class Engine:
         
         class map:
             class textures:
-                scatters = []
                 base = None
                 overlay = None
                 
-                obj_scatter = []
                 obj_base = None
                 obj_overlay = None
+                obj_scatter = []
             name = None
             cfg = None
             rendermethod = None
+            player = None
         self.map = map
     
     def load_map(self, name):
         if os.path.isdir(os.path.join(sys.path[0], 'server', 'maps', name)):
+            #check user cfg
             with open(os.path.join(sys.path[0], 'user', 'config.json'), 'r') as file:
                 self.map.settingscfg = json.load(file)
-                
+            
+            #use correct rendering method
             if self.map.settingscfg['graphics']['PILrender']:
-                self.map.rendermethod = __import__('PIL').ImageTk.PhotoImage
+                self.map.rendermethod = __import__('PIL.ImageTk').ImageTk.PhotoImage
             else:
                 self.map.rendermethod = tk.PhotoImage
         
+            #delete old scatters, background and overlays
             if not self.map.textures.obj_scatter == []:
                 for scatter in self.map.textures.obj_scatter:
                     self.game.canvas.delete(scatter)
@@ -79,28 +82,55 @@ class Engine:
             if not self.map.textures.obj_overlay == None:
                 self.game.canvas.delete(self.map.textures.obj_overlay)
             
+            #open map cfg
             self.map.name = name
             with open(os.path.join(sys.path[0], 'server', 'maps', name, 'list.json'), 'r') as file:
                 self.map.cfg = json.load(file)
             
+            #render base and overlay
             self.map.textures.base = self.map.rendermethod(file = os.path.join(sys.path[0], 'server', 'maps', name, self.map.cfg['background']['base']))
             self.map.textures.overlay = self.map.rendermethod(file = os.path.join(sys.path[0], 'server', 'maps', name, self.map.cfg['background']['overlay']))
             
+            #add base layer
             self.map.textures.obj_base = self.game.canvas.create_image(400, 300, image = self.map.textures.base)
             
+            #add scatters
+            self.map.textures.obj_scatter = []
             for i in range(int(self.map.cfg['background']['scatternum'] / len(self.map.cfg['background']['scatters']))):
                 for scatter in self.map.cfg['background']['scatters']:
                     scattermdl = Model(os.path.join(sys.path[0], 'server', 'maps', name, scatter), self.map.rendermethod, self.game.canvas)
                     scattermdl.setpos(random.randint(0, 800), random.randint(0, 600))
                     self.map.textures.obj_scatter.append(scattermdl)
             
+            #load player
+            self.map.player = Player(os.path.join(sys.path[0], 'server', 'maps', name, self.map.cfg['player']), self)
+            self.map.player.setpos(400, 300)
+            
+            #add overlay
             self.map.textures.obj_overlay = self.game.canvas.create_image(400, 300, image = self.map.textures.overlay)
 
 class Water:
     pass
 
 class Player:
-    pass
+    def __init__(self, path, engine):
+        self.path = path
+        self.engine = engine
+        
+        class pos:
+            x = 0
+            y = 0
+        self.pos = pos
+        
+        self.model = Model(path, self.engine.map.rendermethod, self.engine.game.canvas)
+        self.setpos()
+    
+    def setpos(self, x = None, y = None):
+        if not x == None:
+            self.pos.x = x
+        if not y == None:
+            self.pos.y = y
+        self.model.setpos(self.pos.x, self.pos.y)
 
 class Model:
     def __init__(self, path, imageloader, canvas):
@@ -111,20 +141,35 @@ class Model:
         class graphics:
             x = 0
             y = 0
+            rotation = 0
             displaytype = None
             class flat:
                 texture = None
                 imgobj = None
+                canvobj = None
             class stack:
-                pass
+                class offsets:
+                    x = 0
+                    y = -0.8
+                textures = []
+                imgobjs = []
+                canvobjs = []
         self.graphics = graphics
         
         with open(os.path.join(self.path, 'list.json'), 'r') as file:
             self.config = json.load(file)
         
+        #load textures
         self.graphics.displaytype = self.config['type']
         if self.graphics.displaytype == 'flat':
             self.graphics.flat.texture = self.imageloader(file = os.path.join(self.path, self.config['texture']))
+        elif self.graphics.displaytype == 'stack':
+            if type(self.config['textures']) == list:
+                for img in self.config['textures']:
+                    self.graphics.stack.textures.append(self.imageloader(file = os.path.join(self.path, img)))
+            else:
+                for img in os.listdir(os.path.join(self.path, 'stack')):
+                    self.graphics.stack.textures.append(self.imageloader(file = os.path.join(self.path, 'stack', img)))
         else:
             raise ValueError('Display type \'{}\' doesn\'t exist'.format(self.config['type']))
         self._render()
@@ -134,16 +179,26 @@ class Model:
             self.graphics.x = x
         if not y == None:
             self.graphics.y = y
+            
         if self.graphics.displaytype == 'flat':
-            self.graphics.flat.imgobj = self.canvas.create_image(self.graphics.x, self.graphics.y, image = self.graphics.flat.texture)
+            self.graphics.flat.canvobj = self.canvas.create_image(self.graphics.x, self.graphics.y, image = self.graphics.flat.texture)
+        elif self.graphics.displaytype == 'stack':
+            for tex in self.graphics.stack.textures:
+                self.graphics.stack.canvobjs.append(self.canvas.create_image(self.graphics.x, self.graphics.y, image = tex))
     
-    def setpos(self, x = None, y = None):
+    def setpos(self, x = None, y = None, rotation = None):
         if not x == None:
             self.graphics.x = x
         if not y == None:
             self.graphics.y = y
+            
         if self.graphics.displaytype == 'flat':
-            self.canvas.coords(self.graphics.flat.imgobj, self.graphics.x, self.graphics.y)
+            self.canvas.coords(self.graphics.flat.canvobj, self.graphics.x, self.graphics.y)
+        elif self.graphics.displaytype == 'stack':
+            i = 0
+            for canvobj in self.graphics.stack.canvobjs:
+                self.canvas.coords(canvobj, self.graphics.x + (i * self.graphics.stack.offsets.x), self.graphics.y + (i * self.graphics.stack.offsets.y))
+                i += 1
 
 class DBAccess:
     def __init__(self, address):
