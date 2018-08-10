83626c5658b2fd4e7425402dd31dae60635310b3
seckujdivad <David.a.juckes@tesco.net>
2018-08-10 18:32:14 +0100

    Added popup messages for when a server couldn't be connected to

--- modules/networking.py
+++ modules/networking.py
@@ -1,3 +1,4 @@
+from tkinter import messagebox
 import multiprocessing as mp
 import socket
 import threading
@@ -221,7 +222,15 @@ class Client:
         self.recv_binds = [] #functions to be called when data is received
         
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.connection.connect((self.serverdata.host, self.serverdata.port))
+        
+        try:
+            self.connection.connect((self.serverdata.host, self.serverdata.port))
+        except ConnectionRefusedError:
+            messagebox.showerror('Connection refused', 'The connection was refused by the target machine. Make sure that the IP you are trying to connect to has a server running')
+            raise ConnectionRefusedError()
+        except socket.gaierror:
+            messagebox.showerror('Address not found', 'The server address could not be found. Check it for misspellings and make sure that it is accessible on your network')
+            raise socket.gaierror()
         
         threading.Thread(target = self.recv_loop, name = 'Client receiver loop').start()
     
