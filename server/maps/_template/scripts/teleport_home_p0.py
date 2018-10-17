d6d280b02d49f91e0d228d9d120d28819e3a208f
seckujdivad <David.a.juckes@tesco.net>
2018-10-17 19:14:35 +0100

    Added template map file

---
+++ server/maps/_template/scripts/teleport_home_p0.py
@@ -0,0 +1,10 @@
+class Script:
+    def __init__(self, panel):
+        self.binds = {'when touching': [self.when_touching],
+                      'on enter': [lambda entity: print('entered')],
+                      'on leave': [lambda entity: print('left')]}
+        self.panel = panel
+    
+    def when_touching(self, entity):
+        print(entity.pos.x, entity.pos.y)
+        entity.setpos(400, 100)
