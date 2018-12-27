c08c4a588ce29a7c556ec53263f8e30184045968
seckujdivad <juckes1david@gmail.com>
2018-12-27 17:00:01 +0000

    Made data structure for new model object

--- modules/bettercanvas.py
+++ modules/bettercanvas.py
@@ -1,3 +1,7 @@
+import tkinter as tk
+import os
+import sys
+
 class CanvasController:
     def __init__(self, canvas, game):
         self.canvas = canvas
@@ -7,7 +11,7 @@ class CanvasController:
         self.reserved_args = ['layer']
         
         with open(os.path.join(sys.path[0], 'user', 'layers.json'), 'r') as file:
-            self.layer_config = json.load(file)
+            self.layer_config = json.load(file) #load user defined order for screen items to be rendered in
         
     def create_rectangle(self, *coords, **args):
         'Wrapper function to provide tk canvas-like syntax'
@@ -27,21 +31,21 @@ class CanvasController:
         if not 'layer' in args:
             args['layer'] = 0
         
-        if type(args['layer']) == str:
+        if type(args['layer']) == str: #layer is string, use lookup table
             if args['layer'] in self.layer_config:
                 args['layer'] = self.layer_config[args['layer']]
             else:
-                raise ValueError('Couldn\'t find layer name "{}" in config'.format(args['layer']))
+                raise ValueError('Couldn\'t find layer name "{}" in config'.format(args['layer'])) #layer not in lookup table
             
         while not len(self.layers) >= args['layer'] + 1:
-            self.layers.append([])
+            self.layers.append([]) #make layer if it doesn't exist
         
-        filtered_args = {}
+        filtered_args = {} #remove arguments that are reserved for controller (layer etc) and pass the rest on to the canvas
         for key in args:
             if not key in self.reserved_args:
                 filtered_args[key] = args[key]
         
-        if obj_type == 'rectangle':
+        if obj_type == 'rectangle': #call relevant canvas function
             obj = self.canvas.create_rectangle(*coords, **filtered_args)
         elif obj_type == 'image':
             obj = self.canvas.create_image(*coords, **filtered_args)
@@ -52,7 +56,7 @@ class CanvasController:
         
         self.layers[args['layer']].append({'object': obj})
         
-        ## objects are always created on the top
+        ## objects are always created on the top of their layer
         
         if not len(self.layers) == args['layer'] + 1: ## logic to find next highest tag and move just below it
             next_layer = None
@@ -75,6 +79,7 @@ class CanvasController:
         return obj
     
     def delete(self, obj):
+        'Delete item from canvas'
         to_remove = []
         for a in range(len(self.layers)):
             for b in range(len(self.layers[a])):
@@ -88,7 +93,51 @@ class CanvasController:
             self.layers[a].pop(b)
         
     def coords(self, obj, *coords):
+        'Set the coordinates of something on the canvas'
         self.canvas.coords(obj, *coords)
     
     def itemconfigure(self, obj, **args):
+        'Configure an item on the canvas'
         self.canvas.itemconfigure(obj, **args)
+
+class Model:
+    '''
+    Model:
+    Similar in function to canvas.create_image
+    canvas_controller - CanvasController object to render to
+    ent_name - name of entity in map files
+    map_path - path to map files
+    layer - string or int for canvas controller
+    img_mode - 'pil' or 'tk', PIL requires pillow
+    '''
+    def __init__(self, canvas_controller, ent_name, map_path, layer, img_mode = 'pil'):
+        self.ent_name = ent_name
+        self.map_path = map_path
+        self.canvas_controller = canvas_controller
+        self.layer = layer
+        self.img_mode = img_mode
+        
+        class attributes:
+            class pos: #current coordinates
+                x = 0
+                y = 0
+                
+            rotation = 0 #current rotation (0-359)
+            rotation_steps = 0 #number of different rotations to make
+            
+            transparency = 0 #current transparency (0-255)
+            transparency_steps = 0 #number of different transparencies to make
+            
+            class offset: #amount that each layer of a stacked model is offset by
+                x = 0
+                y = 0
+            
+            images = []
+            imageobjs = [] #image objects that can be used to create 
+            canvasobjs = [] #objects created from imageobjs
+        self.attributes = attributes
+        
+        class cfgs:
+            model = {}
+            user = {}
+        self.cfgs = cfgs
