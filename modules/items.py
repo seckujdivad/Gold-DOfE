e9255cbfd3296f3b10ed1bc7e7cfd456426d0e51
seckujdivad <David.a.juckes@tesco.net>
2019-04-05 11:57:29 +0100

    Added base item script class

--- modules/items.py
+++ modules/items.py
@@ -3,6 +3,7 @@ import os
 
 import modules.bettercanvas
 
+
 class Item(modules.bettercanvas.Model):
     def __init__(self, canvas_controller, item_name, map_path, layer):
         self.item_name = item_name
@@ -15,3 +16,50 @@ class Item(modules.bettercanvas.Model):
         self.cfgs.item = item_cfg
         
         self.attributes.ticket = None
+
+
+class ItemScript:
+    def __init__(self, name, server):
+        self.server = server
+        
+        class attributes:
+            name = None
+            
+            class pos:
+                x = 0
+                y = 0
+            
+            class velocity:
+                x = 0
+                y = 0
+                delay = 0.05
+            
+            class hitbox:
+                shape = None
+                radius = 0
+        self.attributes = attributes
+        
+        class cfgs:
+            current_map = {}
+        self.cfgs = cfgs
+       
+        self.attributes.name = name
+        self.cfgs.current_map = self.server.serverdata.mapdata
+    
+    def inside_map(self):
+        if self.attributes.hitbox.shape == 'circle':
+            clips_x_high = self.attributes.pos.x > self.cfgs.current_map['geometry'][0] + self.attributes.hitbox.radius
+            clips_x_low = self.attributes.pos.x < 0 - self.attributes.hitbox.radius
+            clips_y_high = self.attributes.pos.y > self.cfgs.current_map['geometry'][1] + self.attributes.hitbox.radius
+            clips_y_low = self.attributes.pos.y < 0 - self.attributes.hitbox.radius
+
+            return clips_x_high or clips_x_low or clips_y_high or clips_y_low
+        
+        else:
+            raise ValueError('Hitbox type "{}" not recognised'.format(self.attributes.hitbox.shape))
+
+    def tick(self):
+        self._tick()
+
+    def _tick(self):
+        pass
